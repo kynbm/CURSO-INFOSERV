c0600423f58ae06f955de25cb1c50cb2fbcd0a60
kynbm <kyndermambagnara@hotmail.com>
2022-04-12 19:58:41 -0300

    arquivos ultima aula

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Desenvolvimento PHP</title>
+    <!-- COMENTÁRIOS SERÃO ADICIONADOS COM O ATALHO crtl+; -->
+</head>
+<body>
+    <?php
+    echo "Olá mundo!!";
+    ?>
+</body>
+</html>
