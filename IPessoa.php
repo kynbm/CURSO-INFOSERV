44dd8b92dd3f2357107067e1a05e057c145927e2
kynbm <101676135+kynbm@users.noreply.github.com>
2022-06-21 14:24:46 -0300

    Delete IPessoa.php

--- IPessoa.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-interface IPessoa {
- public function salvar ();
- public function obtertodos ();
- public function obter ();
- public function atualizar ();
- public function excluir ();
-}
