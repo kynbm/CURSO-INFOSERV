eeed24d5115e1688e4c49538a5898ac63a7a41d4
kynbm <101676135+kynbm@users.noreply.github.com>
2022-06-21 14:38:38 -0300

    Create PessoaFisica.php

---
+++ PessoaFisica.php
@@ -0,0 +1,15 @@
+<?php
+
+class Pessoa_Fisica extends Pessoa{
+
+    public $CPF = "";
+    public $RG = "";
+    public $PIS = "";
+    public $gender = "";
+    public $marital_status = "";
+    public $born = "";
+    public $profession = ""; 
+    public $schooling = "";
+
+
+}
